fa039c5368eaf1650a9ea922a75d343198f793a1
travis-johnson <trav.johnson919@gmail.com>
2017-08-29 10:18:45 -0400

    unable to get the new deduct amount to not reflect the totalyBiWeekly

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ function calculate() {
 
     for (var i = 0; i < expInput; i++) {
         total = total + parseInt(exp[i].value) * 12;
-         x = parseInt(sal.value) - total;
+        x = parseInt(sal.value) - total;
 
     }
     document.getElementById('a2').value = x;
     document.getElementById('a3').value = x / 24;
-   
+
 }
 
 function storage() {
     var annual = document.getElementById('a2');
-    var biweekly = document.getElementById('a3'); 
+    var biweekly = document.getElementById('a3');
     localStorage.setItem("Annual", annual.value);
     localStorage.setItem("Biweekly", biweekly.value);
 
@@ -28,20 +28,25 @@ function calcExpns() {
     var desc = document.getElementById('a5').value;
     var price = document.getElementById('a6').value;
     var deduct = totalBiweekly - price;
-    
+
 
     localStorage.setItem("New Amount", deduct);
-    
-    localStorage.setItem(desc , price);
+
+    localStorage.setItem(desc, price);
+
+
+
+
 
 
     //localStorage.setItem(desc.value, price.value);
 
 }
 
+
+
 function reset() {
     document.getElementById("a5").value = "";
     document.getElementById("a6").value = "";
 
 }
-
