95e0d411283173336fe24ae0a179d924ef03b35e
Travis Johnson <trav.johnson919@gmail.com>
2017-09-07 16:54:24 -0400

    Values that are entered into the first form for initial budget is stored with only 2 numbers after the decimal.  It will now also reset the value of the budget on the 15th and 30 of each month. Need to figure out how to account for the 31st as well.

--- script.js
+++ script.js
@@ -7,19 +7,21 @@ function calculate() {
     for (var i = 0; i < expInput; i++) {
         total = total + parseInt(exp[i].value) * 12;
         x = parseInt(sal.value) - total;
-
+        y = parseInt(x / 24);
     }
-    document.getElementById('a2').value = x;
-    document.getElementById('a3').value = x / 24;
+    document.getElementById('a2').value = x.toFixed(2);
+    document.getElementById('a3').value = y.toFixed(2);
+    
 
 }
 
 function storage() {
     var annual = document.getElementById('a2');
     var biweekly = document.getElementById('a3');
+   
     localStorage.setItem("Annual", annual.value);
     localStorage.setItem("Biweekly", biweekly.value);
-
+    localStorage.setItem("Static-BiWeekly", biweekly.value);
 }
 
 function calcExpns() {
@@ -27,14 +29,15 @@ function calcExpns() {
     var desc = document.getElementById('a5').value;
     var price = document.getElementById('a6').value;
     var deduct = totalBiweekly - price;
-
+    //Stores values of expenses in localStorage.
     localStorage.setItem("Biweekly", deduct.toFixed(2));
     localStorage.setItem(desc, price);
     document.getElementById("remaining_budget").innerHTML = "<h3> Remaining Budget:  $" + localStorage.getItem("Biweekly") + "</h3>";
+
 }
 
 
-
+//Resets the input fields after an expense is entered.
 function reset() {
     document.getElementById("a5").value = "";
     document.getElementById("a6").value = "";
@@ -42,7 +45,16 @@ function reset() {
 }
 
 function display() {
-    
     document.getElementById("remaining_budget").innerHTML = "<h3> Remaining Budget:  $" + localStorage.getItem("Biweekly") + "</h3>";
+    var d = new Date();
+    var n = d.getDate();
+    var sb = localStorage.getItem("Static-BiWeekly");
+    //This will reset the value of Biweekly in localStorage to the static value assigned when you first setup your budget.
+    if (n === 15 || n === 30) {
+        localStorage.setItem("Biweekly", sb);
+    }
+
 
 }
+
+
